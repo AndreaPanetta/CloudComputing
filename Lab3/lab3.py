462baa5a8e11efd8078e0c7f2db4c441e38e17fb
AndreaPanetta <C13312461@mydit.ie>
2015-09-30 16:27:23 +0000

    Added if else statement

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -1,9 +1,12 @@
-"""Program to detect if a word is a palindrome or not"""
+#Program to detect if a word is a palindrome or not
 
 pal_string = raw_input("Enter a string: ")
 
 def reverseString(aString):
   return aString[::-1]
 
-
+if pal_string == reverseString(pal_string):
+  print("True")
+else:
+  print("False")
 
