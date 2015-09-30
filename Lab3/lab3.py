72dfa9cbf6810dc678de03e496b5ae1b0325029b
AndreaPanetta <C13312461@mydit.ie>
2015-09-30 16:23:31 +0000

    Initial commit

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -1,10 +1,6 @@
 """Program to detect if a word is a palindrome or not"""
 
-pal_string = raw_input("Enter a string")
-
-"""pal_string = pal_string.lower()
+pal_string = raw_input("Enter a string: ")
 
 
 
-print "True, %s is a palindrome" %(string1)
-print "False, %s is not a palindrome" %(string1)"""
